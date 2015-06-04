14323fcb559d47a4a30d11b1e8a987fec820b04e
pranaypareek <pranay.metal@gmail.com>
2015-06-04 21:31:38 +0530

    adds state descriptive comments

--- dictionary.js
+++ dictionary.js
@@ -3,7 +3,6 @@ var async = require('async');
 
 var i, j, k = 0;
 var arr = [];
-var data = 'a'.charCodeAt();
 
 function convertToChars(data, cb) {
   if(Array.isArray(data)) {
@@ -14,17 +13,33 @@ function convertToChars(data, cb) {
   }
 }
 
+var data = 'a'.charCodeAt();
+
 for(i=0; i<26; i++) {
   var tmp = [];
   tmp.push(data + i);
+  //[a]
 
   for(j=0; j<26; j++) {
     tmp.push(data + j);
+    //[a, a]
 
     for(k=0; k<26; k++) {
       tmp.push(data + k);
+      //[a, a, a]
+
       arr.push(tmp);
+      //[[a, a, a]]
+
       tmp = [];
+
+      /* to avoid:
+        [ 'z', 'y', 'w' ],
+        [ 'z', 'y', 'x' ],
+        [ 'z', 'y', 'y' ],
+        [ 'z', 'y', 'z' ],
+        [ 'z', 'y', 'z', 'a' ],
+      */
       if(k !== 25)
         tmp.push(data + i);
       tmp.push(data + j);
